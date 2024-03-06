8cebffeb13e81b39d432ecb6b75500de74e54cec
Raphaël Denni <65921801+raphaeldenni@users.noreply.github.com>
2024-03-06 22:37:04 +0100

    refactor: reduce the number of comment and remove a nesting level

--- zorglangue_traductor.py
+++ zorglangue_traductor.py
@@ -1,8 +1,12 @@
 # zorglangue-traductor
 # by Raphaël DENNI
 
-# Function that lowers the letters of a string and capitalizes the first letter, if one uppercase letter is present
+
 def lower_upper(temp_list):
+    """
+    Function that lowers the letters of a string and capitalizes the
+    first letter, if one uppercase letter is present.
+    """
     upper = False
 
     for j in temp_list:
@@ -16,10 +20,10 @@ def lower_upper(temp_list):
         ).split(" ")
 
 
-# Function that shifts a punctuation character to the right in a list
-def shift(
-    temp_list, value, position=190000, mate=False
-):  # The position argument is set to a high value to avoid errors
+def shift(temp_list, value, position=190000, mate=False):
+    """
+    Function that shifts a punctuation character to the right in a list
+    """
     if value in temp_list:
         j = temp_list.index(value)
         letter = temp_list[j + 1]
@@ -27,52 +31,49 @@ def shift(
         temp_list.remove(value)
         temp_list.insert(position, value)
 
-        # The following case is specific to the apostrophe character
-        # because it is the only character that is shifted to the left with another character
-        # (e.g. with this, "l'appareil" becomes "l'lierappa" and not "lierappa'l")
         if mate:
             temp_list.remove(letter)
             temp_list.insert(position, letter)
 
 
-# Function that translates a string into Zorglangue
-# Fun fact: Zorglonde is the name of the waves that transform people into Zorglhommes who speak Zorglangue
 def zorglangue_traductor(string):
-    string = string.split(
-        " "
-    )  # Split the string with spaces as separators to be able to reverse each word separately
+    """Function that translates a string into Zorglangue"""
+
+    string = string.split(" ")
     zorg_string = []
 
     punctuation_list = [",", ";", ":", ".", "?", "!"]
 
     # The next lines reverse each word of the string individually because they need to stay in the same order
-    for i in range(0, len(string), 1):
+    for i in range(len(string)):
         temp_list = list(string[i])
 
-        if (
-            len(temp_list) > 1
-        ):  # The next lines are not executed if the word is only one letter long to avoid errors
-            temp_list.reverse()
+        if len(temp_list) < 1:
+            raise ValueError("The word must be at least one letter long")
 
-            # The next lines shift common ponctuations characters to the right after reversing a word,
-            # because the function reverse() reverses these characters in a string
-            # while they must remain at the same place.
-            # This is necessary to correspond to the punctuation of the Zorglangue language (see shift.py)
-            for j in punctuation_list:
-                shift(temp_list, j)
+        temp_list.reverse()
 
-            # The following case is specific to the apostrophe character
-            # because it is the only character that is shifted to the left with another character
-            # (e.g. with this, "l'appareil" becomes "l'lierappa" and not "lierappa'l")
-            shift(temp_list, "'", 0, True)
+        # The next lines shift common ponctuations characters to the right after reversing a word,
+        # because the function reverse() reverses these characters in a string
+        # while they must remain at the same place.
+        # This is necessary to correspond to the punctuation of the Zorglangue language
+        for j in punctuation_list:
+            shift(temp_list, j)
 
-            # The next line makes the letters lowercase and if needed capitalizes the first letter of the new string,
-            # because the function reverse() places the first letter, which is commonly capitalized,
-            # at the end of the word/string.
-            # This is necessary to correspond to the letter capitalization of the Zorglangue language (see lower_upper.py)
-            lower_upper(temp_list)
+        # The following case is specific to the apostrophe character
+        # because it is the only character that is shifted to the left with another character
+        # (e.g. with this, "l'appareil" becomes "l'lierappa" and not "lierappa'l")
+        shift(temp_list, "'", 0, True)
+
+        # The next line makes the letters lowercase and if needed capitalizes the first letter of the new string,
+        # because the function reverse() places the first letter, which is commonly capitalized,
+        # at the end of the word/string.
+        # This is necessary to correspond to the letter capitalization of the Zorglangue language
+        lower_upper(temp_list)
 
         zorg_string.extend(temp_list)
         zorg_string.append(" ")
 
-    return "".join(zorg_string)
+    result = "".join(zorg_string)
+
+    return result
