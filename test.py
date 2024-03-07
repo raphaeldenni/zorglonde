1a99f30cf5af5850d68574b05a2d1e63ba1af104
Raphaël Denni <65921801+raphaeldenni@users.noreply.github.com>
2024-03-07 02:13:38 +0100

    ci: update test to add reverse process

--- test.py
+++ test.py
@@ -3,3 +3,7 @@ from zorglonde import zorglonde
 
 def test_zorglonde():
     assert zorglonde("Hello world! L'effort.") == "Olleh dlrow! L'troffe."
+
+
+def test_reverse_zorglonde():
+    assert zorglonde("Olleh dlrow! L'troffe.", reverse=True) == "Hello world! L'effort."
